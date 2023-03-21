a2d96f01621c5cd2daa0a2f90d15735de82d7c76
ankitcool123 <ankit.@newvision-software.com>
2023-03-21 14:13:12 +0530

    added

---
+++ src/component/nav/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item active">
+          <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Features</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Pricing</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  );
+}
+
+export default Navbar;
