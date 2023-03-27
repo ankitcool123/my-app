5c807a9fd1fa901e58dcf392e2ff346c8a3a6661
ankitcool123 <ankit.@newvision-software.com>
2023-03-27 21:01:04 +0530

    added new changes on Ui side

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './component/nav/Navbar';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Login from './component/nav/Login';
+import SignUp from './component/nav/SignUp';
 
 function App() {
+    const [SignIn, setSignIn]= useState(false);
   return (
-    <div className="App">
-    <Navbar></Navbar>
-    </div>
+   <BrowserRouter>
+      <Routes>
+        <Route  path="/" element={<Login setSignIn = {setSignIn}/>}></Route> 
+         <Route  path="/Nav" element={<Navbar />}></Route>
+         <Route path="/Reg"  element={<SignUp/>}></Route> 
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/component/nav/Navbar.tsx
+++ src/component/nav/Navbar.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import "./Navbar.css";
 
-function Navbar() {
+
+function Navbar(props: any) {
+  let navigate = useNavigate();
+
+  const onOut = () => {
+    localStorage.clear();
+    navigate("/");
+    window.location.reload()
+
+  };
+
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item active">
-          <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="#">Disabled</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">Dating App</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Machtes</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">List</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Massages</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <div className="Logout">
+          <div onClick={onOut}>LogOut</div>
+        </div>
+      </div>
+    </nav>
+
   );
 }
 
